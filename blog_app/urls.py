5683f4a426d433d09dbc157662e9aff35d03151d
Tonisha Baker <bakertoni17gmail.com>
2019-09-26 22:09:53 +0200

    Add logout fuctuinality.

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # 127.0.0.1:8000/accounts/login --> local
+    # mydjangosite.com/accounts/login --> online
+    path('accounts/login/', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+
+    # 127.0.0.1:8000/accounts/logout --> local
+    # mydjangosite.com/accounts/logout --> online
+    path('accounts/logout/', view=auth_views.LogoutView.as_view(template_name="registration/login.html"), name="logout", kwargs={'next_page': 'post_list'}),
+
+
+    # 127.0.0.1:8000
     path('', include('blog.urls')),
 ]
