9c3aae4086afbf2fb0c623fb02581f6b6a6be4ae
Tonisha Baker <bakertoni17gmail.com>
2019-11-10 21:09:20 +0100

    heroku config update.

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' 
+STATICFILES_STORAGE ='whitnoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
